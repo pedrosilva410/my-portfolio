263e27d7adfc3af861f82beab28835d6a2ad229f
pedrosilva <pedrogdsilva410@gmail.com>
2020-08-27 23:17:28 +0100

    Header Updated

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -8,28 +8,31 @@ const Header = () => {
   console.log(typeof location.pathname);
 
   return (
-    <ul className="nav">
-      <li>
-        <NavLink exact className="nav_item" activeClassName="nav_item_selected" to="/">
-          Home
-        </NavLink>
-      </li>
-      <li>
-        <NavLink className="nav_item" activeClassName="nav_item_selected" to="/about">
-          About
-        </NavLink>
-      </li>
-      <li>
-        <NavLink exact className="nav_item" activeClassName="nav_item_selected" to="/projects">
-          Projects
-        </NavLink>
-      </li>
-      <li>
-        <NavLink className="nav_item" activeClassName="nav_item_selected" to="/contact">
-          Contact
-        </NavLink>
-      </li>
-    </ul>
+    <header className="header">
+      <ul className="nav">
+        <li>
+          <NavLink exact className="nav_item" activeClassName="nav_item_selected" to="/">
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink className="nav_item" activeClassName="nav_item_selected" to="/about">
+            About
+          </NavLink>
+        </li>
+        <li>
+          <NavLink exact className="nav_item" activeClassName="nav_item_selected" to="/projects">
+            Projects
+          </NavLink>
+        </li>
+      </ul>
+
+      <div className="header__contact_container">
+          <NavLink className="header__contact" activeClassName="nav_item_selected" to="/contact">
+            Contact
+          </NavLink>
+          </div>
+    </header>
   );
 };
 
