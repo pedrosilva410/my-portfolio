9891f5ae9a98ba026bf46220213e4e786c330e6a
pedrosilva <pedrogdsilva410@gmail.com>
2020-08-28 11:45:00 +0100

    Home and Contact final versions

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,25 +1,34 @@
 import React from "react";
 import "./styles.css";
 
-import twitter from "../../assets/twitter.svg"
-import github from "../../assets/github.svg"
+import twitter from "../../assets/twitter.svg";
+import github from "../../assets/github.svg";
 
 const Contact = () => {
   return (
-    <main>
-      <h1>Contact</h1>
-      <p className="contact__p">
-        ( You can find me here )
-      </p>
-      <div className="contact__contacts">
-        <a href="https://twitter.com/pedrogdsilva410" rel="noopener noreferrer"  target="_blank">
-        <img src={twitter} alt="twitter" className="contact__img"/>
-        </a>
+    <main className="contact__wrapper">
+      <div className="contact__main">
+        <h1>Contact</h1>
+        <p className="contact__p">( You can find me here )</p>
+        <div className="contact__contacts">
+          <a
+            href="https://twitter.com/pedrogdsilva410"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <img src={twitter} alt="twitter" className="contact__img" />
+          </a>
 
-        <a href="https://github.com/pedrosilva410" rel="noopener noreferrer"  target="_blank">
-        <img src={github} alt="github" className="contact__img"/>
-        </a>
+          <a
+            href="https://github.com/pedrosilva410"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <img src={github} alt="github" className="contact__img" />
+          </a>
+        </div>
       </div>
+      <div className="contact__section_making_space" />
     </main>
   );
 };
