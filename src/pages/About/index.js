f60e6542c055542d4e289fef8f83ea3e2c6c6b80
Pedro <35297069+pedrosilva410@users.noreply.github.com>
2021-06-10 20:45:06 +0200

    styled about page
    gave some styling in the about page and fixed some typos

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -5,32 +5,42 @@ const About = () => {
   return (
     <main className="about__wrapper">
  <div className="about__main">
-        <h1 className="about__title">I'm guessing you wanted to know more...</h1>
-        <p className="about__text">
 
+        <h1 className="about__title">I'm guessing you wanted to know more...</h1>
+
+        <p className="about__text">
         I always had an enormous interest in computers since a young age,
         how they worked and what could we really do with them, so when it
         was offered me a chance to study programming and business in my
         last 3 years of school I didn't had to think twice.
-        <br/>
+        </p>
+
+        <p className="about__text">
         During those 3 years I learnt a lot about programming, from pseudo
         code and little apps in pascal to a fully functional website.
         I was very happy with what I had achieved, but I wanted more...
-        <br/>
-        Ultimately, school ended I was a little bit lost because I didn't
+        </p>
+
+        <p className="about__text">
+        Ultimately, school ended and I was a little bit lost because I didn't
         want to go to collage but I wasn't ready for a programming job yet,
-        and thats when I got to know mindera, a big technology company in 
-        Portugal and worldwide, they had a internship program where I would 
-        learn about any area of programming I would have interest in.
-        <br/>
+        and that's when I got to know mindera, a big technology company in 
+        Portugal and worldwide, they had an internship program where I would 
+        learn about any area of programming I had interest in.
+        </p>
+
+        <p className="about__text">
         The program endured a year. It changed my life. I learned a lot,
         made a bunch of mistakes, but in overall I got much knowledge
         there both at programming expertise but also as a person
         and learnt that the area that I genuinely have interest 
         in is web development and design.
-        <br/>
+        </p>
+
+        <p className="about__text">
         This is it so far, let's see what the world has prepared for me...
         </p>
+
       </div>
       <div className="about__section_making_space" />
     </main>
